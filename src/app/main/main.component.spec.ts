5989ef7a4b9fc817e6a815d32081d7babc7a944a
Matt <m.bimpson@gmail.com>
2019-05-11 11:37:11 -0500

    add test for adding new todo

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainComponent } from './main.component';
+import { FormsModule } from '@angular/forms';
+import { TodoComponent } from '../todo/todo.component';
+import { ConfirmBtnComponent } from '../confirm-btn/confirm-btn.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-describe('MainComponent', () => {
+fdescribe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      imports: [FormsModule],
+      declarations: [ MainComponent, TodoComponent, ConfirmBtnComponent ]
     })
     .compileComponents();
   }));
@@ -22,4 +27,13 @@ describe('MainComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be able to add a todo item', () => {
+    const textBox = fixture.debugElement.query(By.css('input[type=text]'));
+    textBox.nativeElement.value = 'test';
+    const btnAdd = fixture.nativeElement.querySelector('#btnAdd');
+    btnAdd.disabled = false;
+    btnAdd.click();
+    expect(component.todos.length).toEqual(1);
+  })
 });
