fe3d857b428f0290aa31004e01c631f28422c6b0
Matt <m.bimpson@gmail.com>
2019-05-13 16:41:00 -0500

    firebase config

--- src/environments/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export default {
-	apiKey: '<your-api-key>',
-	authDomain: '<your-auth-domain>',
-	databaseURL: '<your-database-URL>',
-	projectId: '<your-project-id>',
-	storageBucket: '<your-storage-bucket>',
-	messagingSenderId: '<your-messaging-sender-id>'
-}
